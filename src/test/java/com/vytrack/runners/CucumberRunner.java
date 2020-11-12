6927ace61dbd80def1cf51034b3ec11ae2fc1d2a
tenus88 <diachenkoandrei1@gmail.com>
2020-11-11 23:12:55 -0500

    Project(Vy Truck with Vasil)

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "@negative_login",
+        tags = "@smoke",
         publish = true
 )
 public class CucumberRunner {
--- src/test/java/com/vytrack/utils/Driver.java
+++ src/test/java/com/vytrack/utils/Driver.java
@@ -39,11 +39,12 @@ public class Driver {
                     driver = new FirefoxDriver();
                     break;
                 case "remote-chrome":
+
                     try {
 //                    ChromeOptions chromeOptions = new ChromeOptions();
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
-                        URL gridUrl = new URL("http://3.82.5.142:4444/wd/hub");
+                        URL gridUrl = new URL("http://54.208.214.61:4444/wd/hub");
                         driver = new RemoteWebDriver(gridUrl, desiredCapabilities);
                     } catch (Exception e) {
                         e.printStackTrace();
