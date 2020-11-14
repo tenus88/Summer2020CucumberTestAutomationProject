0dd1349444eb59f41aee737cb99688ef51f8bfca
tenus88 <diachenkoandrei1@gmail.com>
2020-11-13 19:29:48 -0500

    Last update5

--- src/test/java/com/vytrack/utils/Driver.java
+++ src/test/java/com/vytrack/utils/Driver.java
@@ -43,7 +43,7 @@ public class Driver {
 //                    ChromeOptions chromeOptions = new ChromeOptions();
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
-                        URL gridUrl = new URL("http://54.208.214.61:4444/wd/hub");
+                        URL gridUrl = new URL("http://3.82.5.142:4444/wd/hub");
                         driver = new RemoteWebDriver(gridUrl, desiredCapabilities);
                     } catch (Exception e) {
                         e.printStackTrace();
